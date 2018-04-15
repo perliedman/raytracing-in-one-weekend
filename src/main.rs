18ab77b836b2c9ce21484d4d268c3fba2886a2a6
Per Liedman <per.liedman@dotnetmentor.se>
2018-04-15 23:17:07 +0200

    Working Vec3

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,11 @@ fn main() {
 
   for j in (0..ny).rev() {
     for i in 0..nx {
-      let r = (i as f64) / (nx as f64);
-      let g = (j as f64) / (ny as f64);
-      let b = 0.2;
+      let col = vec3::build_vec3((i as f32) / (nx as f32), (j as f32) / (ny as f32), 0.2);
 
-      let ir = (255.99 * r) as i32;
-      let ig = (255.99 * g) as i32;
-      let ib = (255.99 * b) as i32;
+      let ir = (255.99 * col[0]) as i32;
+      let ig = (255.99 * col[1]) as i32;
+      let ib = (255.99 * col[2]) as i32;
 
       println!("{} {} {}", ir, ig, ib);
     }
